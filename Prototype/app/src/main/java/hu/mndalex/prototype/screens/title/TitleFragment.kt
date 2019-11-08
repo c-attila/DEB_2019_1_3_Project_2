6b0b818a395c94fdc5d1cb7817c63af793537204
Alex Andras Mandrik <alexvok12@gmail.com>
2019-11-08 20:56:38 +0100

    Prototype: Title fragment hozzáadva

---
+++ Prototype/app/src/main/java/hu/mndalex/prototype/screens/title/TitleFragment.kt
@@ -0,0 +1,27 @@
+package hu.mndalex.prototype.screens.title
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import hu.mndalex.prototype.R
+import hu.mndalex.prototype.databinding.TitleFragmentBinding
+
+class TitleFragment : Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        val binding: TitleFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.title_fragment, container, false)
+
+//        binding.playGameButton.setOnClickListener {
+//            findNavController().navigate(TitleFragmentDirections.actionTitleToGame())
+//        }
+        return binding.root
+    }
+
+}
