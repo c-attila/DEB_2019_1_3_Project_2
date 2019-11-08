e40c4c4a229c6487b9258af2e7f977a2028768ee
Alex Andras Mandrik <alexvok12@gmail.com>
2019-11-08 18:40:33 +0100

    Prototype init

---
+++ Prototype/app/src/main/java/hu/mndalex/prototype/MainActivity.kt
@@ -0,0 +1,19 @@
+package hu.mndalex.prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.mndalex.prototype.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
