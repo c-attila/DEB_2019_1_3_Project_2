dab9b7498548a2291fbc277928b04b32ff6a4349
Alex Andras Mandrik <alexvok12@gmail.com>
2019-11-12 15:34:42 +0100

    Prototype: Kezdő pozició meghatározása megírva

--- Prototype/app/src/main/java/hu/mndalex/prototype/screens/game/GameMode1Fragment.kt
+++ Prototype/app/src/main/java/hu/mndalex/prototype/screens/game/GameMode1Fragment.kt
@@ -1,35 +1,32 @@
 package hu.mndalex.prototype.screens.game
 
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.util.TypedValue
-import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.children
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import hu.mndalex.prototype.R
 import hu.mndalex.prototype.databinding.GameMode1FragmentBinding
-import org.w3c.dom.Text
 
-class GameMode1Fragment : Fragment(){
+class GameMode1Fragment : Fragment() {
 
     private lateinit var binding: GameMode1FragmentBinding
 
     private val TABLE_WIDTH = 7
     private val TABLE_HEIGHT = 8
 
-    private val cellTextList = listOf("Hotel","Gas Station", "Restaurant")
+    private val INVISIBLE_CELL_TEXT_SIZE = 0F
+    private val VISIBLE_CELL_TEXT_SIZE = 12F
+
+    private val START_CELL_TEXT = "Start"
+
+    private val cellTextList =
+        listOf("Hotel", "Gas Station", "Restaurant", "Shop", "Casino", "Bakery")
 
 
     override fun onCreateView(
@@ -38,24 +35,56 @@ class GameMode1Fragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.game_mode_1_fragment,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.game_mode_1_fragment, container, false)
 
-        generateCells()
+//        generateCells()
+        setStartPos()
 
         return binding.root
     }
 
-    private fun generateCells(){
+    private fun generateCells(posX: Int, posY: Int) {
 
-        for (i in 0 until TABLE_HEIGHT){
-            for(j in 0 until TABLE_WIDTH){
-                val row = binding.tableLayout.getChildAt(i) as TableRow
-                val cell = row.getChildAt(j) as TextView
-                cell.text  = cellTextList.shuffled().take(1)[0]
-                cell.textSize = 0F
-            }
+        var row = binding.tableLayout.getChildAt(posY) as TableRow
+        var cell: TextView
+        if (posX < TABLE_WIDTH - 1) {
+            cell = row.getChildAt(posX + 1) as TextView
+            if (cell.text.isEmpty())
+                cell.text = cellTextList.shuffled().take(1)[0]
+        }
+        if (posX > 0) {
+            cell = row.getChildAt(posX - 1) as TextView
+            if (cell.text.isEmpty())
+                cell.text = cellTextList.shuffled().take(1)[0]
         }
 
+        if (posY > 0) {
+            row = binding.tableLayout.getChildAt(posY - 1) as TableRow
+            cell = row.getChildAt(posX) as TextView
+            if (cell.text.isEmpty())
+                cell.text = cellTextList.shuffled().take(1)[0]
+        }
+
+        if (posY < TABLE_HEIGHT - 1) {
+            row = binding.tableLayout.getChildAt(posY + 1) as TableRow
+            cell = row.getChildAt(posX) as TextView
+            if (cell.text.isEmpty())
+                cell.text = cellTextList.shuffled().take(1)[0]
+        }
+
+    }
+
+    private fun setStartPos() {
+
+        val posX = (0 until TABLE_WIDTH).random()
+        val posY = (0 until TABLE_HEIGHT).random()
+
+        val row = binding.tableLayout.getChildAt(posY) as TableRow
+        val cell = row.getChildAt(posX) as TextView
+        cell.text = START_CELL_TEXT
+        cell.setBackgroundColor(resources.getColor(R.color.player_cell_background_color))
+
+        generateCells(posX, posY)
     }
 
 
