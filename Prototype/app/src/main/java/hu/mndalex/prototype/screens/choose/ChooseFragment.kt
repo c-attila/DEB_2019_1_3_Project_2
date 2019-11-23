0e5b23e60a29512d4bb03ff30ce7d694bf24ee43
kovacsattila <kovacsat2000@gmail.com>
2019-11-23 17:56:09 +0100

    Játékosszám választó megírva.

--- Prototype/app/src/main/java/hu/mndalex/prototype/screens/choose/ChooseFragment.kt
+++ Prototype/app/src/main/java/hu/mndalex/prototype/screens/choose/ChooseFragment.kt
@@ -1,4 +1,4 @@
-package hu.mndalex.prototype.screens.play
+package hu.mndalex.prototype.screens.choose
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,21 +8,23 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import hu.mndalex.prototype.R
+import hu.mndalex.prototype.databinding.ChooseFragmentBinding
 import hu.mndalex.prototype.databinding.PlayFragmentBinding
+import hu.mndalex.prototype.screens.play.PlayFragmentDirections
 
-class PlayFragment : Fragment (){
+class ChooseFragment : Fragment (){
 
-    lateinit var binding: PlayFragmentBinding
+    lateinit var binding: ChooseFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.play_fragment,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.choose_fragment,container,false)
 
         binding.playButton1.setOnClickListener{findNavController()
-            .navigate(PlayFragmentDirections.actionPlayDestinationToChooseFragment())}
+            .navigate(ChooseFragmentDirections.actionChooseFragmentToGameDestination())}
 
         return binding.root
     }
