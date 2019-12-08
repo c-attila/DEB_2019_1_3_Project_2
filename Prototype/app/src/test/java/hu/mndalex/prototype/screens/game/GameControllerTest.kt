33ede0e0a48450b51fa8f9a525d314f42f044226
Alex Andras Mandrik <alexvok12@gmail.com>
2019-12-08 10:27:05 +0100

    GameControllerTest class létrehozva

---
+++ Prototype/app/src/test/java/hu/mndalex/prototype/screens/game/GameControllerTest.kt
@@ -0,0 +1,13 @@
+package hu.mndalex.prototype.screens.game
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class GameControllerTest{
+
+    @Test
+    fun testPayTax() {
+        
+    }
+
+}
